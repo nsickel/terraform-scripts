0a7df70e454c4507aff819f3a3428e2a21f9b5e3
Nils Sickel <nils.sickel@gmail.com>
2019-03-14 22:39:27 +0100

    add policie assignment for groups

--- iam-user-group-assignment/main.tf
+++ iam-user-group-assignment/main.tf
@@ -4,20 +4,30 @@ provider "aws" {
   region = "eu-central-1"
 }
 
-resource "aws_iam_user" "user" {
-  count = "${length(var.users)}"
-  name = "${element(var.users,count.index)}"
-}
-
+# create groups
 resource "aws_iam_group" "group" {
   count = "${length(var.groups)}"
-  name = "${element(var.groups,count.index)}"
+  name  = "${element(var.groups,count.index)}"
 }
 
+# attach policies to groups
+resource "aws_iam_group_policy_attachment" "group_policy" {
+  count       = "${length(var.group_policies)}"
+  group       = "${element(aws_iam_group.group.*.name,index(var.groups,element(keys(var.group_policies[count.index]),0)))}"
+  policy_arn  = "${element(values(var.group_policies[count.index]),0)}"
+}
+
+# create users
+resource "aws_iam_user" "user" {
+  count = "${length(var.users)}"
+  name  = "${element(var.users,count.index)}"
+}
+
+# add users to groups
 resource "aws_iam_group_membership" "group_membership" {
   count = "${length(var.groups)}"
   name  = "${element(var.groups,count.index)}-membership"
-  users = "${var.user_groups[element(var.groups,count.index)]}"
+  users = "${var.group_users[element(var.groups,count.index)]}"
   group = "${element(aws_iam_group.group.*.name,count.index)}"
 }
 
